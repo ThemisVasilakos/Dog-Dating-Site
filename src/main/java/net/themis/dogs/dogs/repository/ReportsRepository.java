00fddec5761f44b7bd522d7f172572de695d3fbc
ThemisVasilakos <80680153+ThemisVasilakos@users.noreply.github.com>
2023-04-15 15:40:13 +0300

    Delete src directory

--- src/main/java/net/themis/dogs/dogs/repository/ReportsRepository.java
+++
@@ -1,7 +0,0 @@
-package net.themis.dogs.dogs.repository;
-
-import net.themis.dogs.dogs.model.Reports;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportsRepository extends JpaRepository<Reports,Long> {
-}
